75f22e7a6e89bd8c1cbe3cc1ecfc1c1d2c8eb47a
Surinam <extensionsapp@gmail.com>
2017-11-24 15:16:16 +0200

    Add Stop words

--- config/default/config.js
+++ config/default/config.js
@@ -43,7 +43,8 @@ module.exports = {
             "country": "",
             "actor": "",
             "director": "",
-            "query_id": ""
+            "query_id": "",
+            "search": ""
         }
     },
     "default": {
