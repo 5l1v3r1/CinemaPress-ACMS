503a8d60e0acc1ed875e5b8b5caffd36a6682fb5
Surinam <extensionsapp@gmail.com>
2017-11-25 20:53:04 +0200

    Add cached RSS

--- routes/rss.js
+++ routes/rss.js
@@ -4,6 +4,7 @@
  * Module dependencies.
  */
 
+var CP_cache  = require('../lib/CP_cache');
 var CP_get    = require('../lib/CP_get.min');
 var CP_regexp = require('../lib/CP_regexp');
 
@@ -18,6 +19,7 @@ var modules = require('../config/production/modules');
  * Node dependencies.
  */
 
+var md5     = require('md5');
 var express = require('express');
 var router  = express.Router();
 
@@ -25,87 +27,175 @@ var router  = express.Router();
  * RSS.
  */
 
-router.get('/?', function(req, res) {
+router.get('/?', function(req, res, next) {
 
-    var render = {};
-    render.config = config;
-    render.movies = [];
-    var collection = (req.query.collection) ? CP_regexp.str(req.query.collection) : '';
+    var url = req.originalUrl;
+    var urlHash = md5(url.toLowerCase());
 
-    if (!modules.rss.status) {
-        res.header('Content-Type', 'application/xml');
-        return res.render('desktop/rss', render);
+    getRender(function (err, render) {
+
+        renderData(err, render);
+
+    });
+
+    /**
+     * Get render.
+     *
+     * @param {Callback} callback
+     */
+
+    function getRender(callback) {
+
+        return (config.cache.time)
+            ? getCache(
+                function (err, render) {
+                    return (err)
+                        ? callback(err)
+                        : callback(null, render)
+                })
+            : getSphinx(
+                function (err, render) {
+                    return (err)
+                        ? callback(err)
+                        : callback(null, render)
+                });
+
     }
 
-    if (modules.content.status && collection) {
-        CP_get.contents(
-            {"content_url": collection},
-            function (err, contents) {
-                if (err) {
-                    res.status(404).render('error', {
-                        "search"  : config.urls.search,
-                        "status"  : 404,
-                        "message" : err
-                    });
-                    return;
-                }
-                if (contents && contents.length && contents[0].movies) {
-                    var query_id = [];
-                    contents[0].movies.forEach(function (item, i, arr) {
-                        query_id.push(item + '^' + (parseInt(arr.length) - parseInt(i)))
-                    });
-                    var query = {"query_id": query_id.join('|')};
-                    CP_get.movies(
-                        query,
-                        contents[0].movies.length,
-                        '',
-                        1,
-                        function (err, movies) {
-                            if (err) {
-                                res.status(404).render('error', {
-                                    "search": config.urls.search,
-                                    "status": 404,
-                                    "message": err
-                                });
-                                return;
-                            }
+    /**
+     * Get cache.
+     *
+     * @param {Callback} callback
+     */
 
-                            render.movies = movies;
-                            res.header('Content-Type', 'application/xml');
-                            res.render('desktop/rss', render);
-                        });
-                }
-                else {
-                    res.status(404).render('error', {
-                        "search"  : config.urls.search,
-                        "status"  : 404,
-                        "message" : "Коллекция пустая!"
+    function getCache(callback) {
+
+        CP_cache.get(urlHash, function (err, render) {
+
+            if (err) return callback(err);
+
+            return (render)
+                ? callback(null, render)
+                : getSphinx(
+                    function (err, render) {
+                        return (err)
+                            ? callback(err)
+                            : callback(null, render)
                     });
+
+        });
+
+    }
+
+    /**
+     * Get sphinx.
+     *
+     * @param {Callback} callback
+     */
+
+    function getSphinx(callback) {
+
+        if (!modules.rss.status) {
+            return callback('RSS отключен!');
+        }
+
+        var render = {};
+        render.config = config;
+        render.movies = [];
+        var collection = (req.query.collection) ? CP_regexp.str(req.query.collection) : '';
+
+        if (modules.content.status && collection) {
+            CP_get.contents(
+                {"content_url": collection},
+                function (err, contents) {
+                    if (err) {
+                        return callback(err);
+                    }
+                    if (contents && contents.length && contents[0].movies) {
+                        var query_id = [];
+                        contents[0].movies.forEach(function (item, i, arr) {
+                            query_id.push(item + '^' + (parseInt(arr.length) - parseInt(i)))
+                        });
+                        var query = {"query_id": query_id.join('|')};
+                        CP_get.movies(
+                            query,
+                            contents[0].movies.length,
+                            '',
+                            1,
+                            function (err, movies) {
+                                if (err) {
+                                    return callback(err);
+                                }
+
+                                render.movies = movies;
+                                callback(null, render);
+                            });
+                    }
+                    else {
+                        return callback('Коллекция пустая!');
+                    }
+                });
+        }
+        else {
+            CP_get.publishIds(true, function (err, ids) {
+                if (err) {
+                    return callback(err);
+                }
+                else if (!ids) {
+                    return callback('Публикация окончена!');
+                }
+                render.movies = ids.movies;
+                callback(null, render);
+            });
+        }
+
+    }
+
+    /**
+     * Render data.
+     *
+     * @param {Object} err
+     * @param {Object} render
+     */
+
+    function renderData(err, render) {
+
+        if (err) {
+            console.log('[routes/rss.js] Error:', url, err);
+
+            return next({
+                "status": 404,
+                "message": err
+            });
+        }
+
+        if (typeof render === 'object') {
+
+            res.header('Content-Type', 'application/xml');
+            res.render('desktop/rss', render, function(err, html) {
+                if (err) console.log('[renderData] Render Error:', err);
+
+                res.send(html);
+
+                if (config.cache.time && html) {
+                    CP_cache.set(
+                        urlHash,
+                        html,
+                        config.cache.time,
+                        function (err) {
+                            if (err) console.log('[renderData] Cache Set Error:', err);
+                        }
+                    );
                 }
             });
-    }
-    else {
-        CP_get.publishIds(true, function (err, ids) {
-            if (err) {
-                res.status(404).render('error', {
-                    "search"  : config.urls.search,
-                    "status"  : 404,
-                    "message" : err
-                });
-                return;
-            }
-            else if (!ids) {
-                res.status(404).render('error', {
-                    "search"  : config.urls.search,
-                    "status"  : 404,
-                    "message" : "Публикация окончена!"
-                });
-                return;
-            }
-            render.movies = ids.movies;
-            res.header('Content-Type', 'application/xml');
-            res.render('desktop/rss', render);
-        });
+
+        }
+        else {
+
+            res.send(render);
+
+        }
+
     }
 
 });
