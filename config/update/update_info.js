77595af21be5c75d0aab8b1eb43490875d1291b0
Surinam <extensionsapp@gmail.com>
2018-01-04 22:39:29 +0200

    Add update default info

---
+++ config/update/update_info.js
@@ -0,0 +1,59 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+
+var CP_save = require('../../lib/CP_save.min');
+var CP_get  = require('../../lib/CP_get.min');
+
+/**
+ * Node dependencies.
+ */
+
+var async = require('async');
+
+(function upd(i) {
+
+    i = i || 1;
+    var ii = i+1;
+
+    if (i >= 300) {
+        return;
+    }
+
+    CP_get.movies(
+        {"certainly": true},
+        10000,
+        '',
+        i,
+        false,
+        function (err, movies) {
+            if (err) {
+                console.log(err);
+                return;
+            }
+
+            if (movies && movies.length) {
+                async.eachOfLimit(movies, 1, function (movie, key, callback) {
+                    delete movie.year;
+                    delete movie.premiere;
+                    delete movie.kp_rating;
+                    delete movie.kp_vote;
+                    delete movie.imdb_rating;
+                    delete movie.imdb_vote;
+                    movie.id = movie.kp_id;
+                    CP_save.save(
+                        movie,
+                        'rt',
+                        function (err, result) {
+                            return callback(err);
+                        });
+                }, function (err) {
+                    console.log(err);
+                    upd(ii);
+                });
+            }
+        });
+
+})();
